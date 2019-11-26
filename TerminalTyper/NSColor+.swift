a0dc66e447858980674ea8b102654be156b32285
Daniel Bonates <daniel@bonates.com>
2019-11-25 21:14:47 -0300

    change default dark color

--- TerminalTyper/NSColor+.swift
+++ TerminalTyper/NSColor+.swift
@@ -13,5 +13,5 @@ extension NSColor {
     static var darkText: NSColor { return #colorLiteral(red: 0.2156862745, green: 0.2156862745, blue: 0.2156862745, alpha: 1) }
 
     static var lightBackground: NSColor { return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) }
-    static var darkBackground: NSColor { return #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1) }
+    static var darkBackground: NSColor { return #colorLiteral(red: 0.1601257622, green: 0.1645516157, blue: 0.1861833036, alpha: 1) }
 }
